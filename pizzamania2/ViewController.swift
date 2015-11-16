340b68cbe7d3261d11d0e7eab53e0232aed42c44
pako10 <pakoAndrade@eduardos-macbook-pro.local>
2015-11-15 19:54:19 -0600

    Initial Commit

---
+++ pizzamania2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pizzamania2
+//
+//  Created by Francisco Humberto Andrade Gonzalez on 15/11/15.
+//  Copyright © 2015 Francisco Humberto Andrade Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
